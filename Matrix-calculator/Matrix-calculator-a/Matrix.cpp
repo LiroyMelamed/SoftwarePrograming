fa5ad7962931788c123036322906ffe9920159b8
Liroy <zaqwedsxc2@gmail.com>
2022-05-01 16:32:30 +0300

    check 3

--- Matrix-calculator/Matrix-calculator-a/Matrix.cpp
+++ Matrix-calculator/Matrix-calculator-a/Matrix.cpp
@@ -19,32 +19,70 @@ namespace zich
  Matrix Matrix::operator+(const Matrix &_mat) const
  {
   check_Matrix_size(_mat, 1);
-  return *this;
+  vector<double> temp((size_t)(_size));
+
+  for (size_t i = 0; i < _size; i++)
+  {
+   temp[i] = _mat[i] + mat._mat[i];
+  }
+  return Matrix(temp, _row, _col);
  }
 
  Matrix Matrix::operator-(const Matrix &_mat) const
  {
   check_Matrix_size(_mat, 1);
-
-  return *this;
+  vector<double> temp((size_t)(_size));
+  for (size_t i = 0; i < _size; ++i)
+  {
+   temp[i] = _mat[i] - mat._mat[i];
+  }
+  return Matrix(temp, _row, _col);
  }
 
  Matrix Matrix::operator*(const Matrix &_mat) const
  {
   check_Matrix_size(_mat, 2);
+  vector<double> temp((size_t)(_row * mat._col));
 
-  return *this;
+  for (size_t i = 0; i < _row; ++i)
+  {
+   for (size_t j = 0; j < mat._col; ++j)
+   {
+    uint index = i * (size_t)mat._col + j;
+    temp[index] = 0;
+    for (size_t k = 0; k < mat._row; ++k)
+    {
+     uint row = i * (size_t)_col + k;
+     uint col = k * (size_t)mat._col + j;
+     temp[index] += _mat[row] * mat._mat[col];
+    }
+   }
+  }
+  return Matrix(temp, _row, mat._col);
  }
 
  Matrix Matrix::operator*(double num) const
  {
-
-  return *this;
+  vector<double> temp((size_t)(_size));
+  for (size_t i = 0; i < _size; ++i)
+  {
+   temp[i] = _mat[i] * num;
+  }
+  return Matrix(temp, _row, _col);
  }
 
  Matrix Matrix::operator-()
  {
-  return *this;
+  vector<double> temp((size_t)(_size));
+  for (size_t i = 0; i < _size; ++i)
+  {
+   if (_mat[i] == 0)
+   {
+    continue;
+   }
+   temp[i] = _mat[i] * -1;
+  }
+  return Matrix(temp, _row, _col)
  }
 
  Matrix Matrix::operator+()
@@ -54,106 +92,217 @@ namespace zich
 
  Matrix &Matrix::operator++()
  {
+  for (size_t i = 0; i < _size; ++i)
+  {
+   this->_mat[i]++;
+  }
   return *this;
  }
 
  Matrix Matrix::operator++(int)
  {
-  return *this;
+  Matrix temp = *this;
+  for (size_t i = 0; i < _size; ++i)
+  {
+   ++this->_mat[i];
+  }
+  return temp;
  }
 
  Matrix &Matrix::operator--()
  {
+  for (size_t i = 0; i < _size; ++i)
+  {
+   this->_mat[i]--;
+  }
   return *this;
  }
 
  Matrix Matrix::operator--(int)
  {
-  return *this;
+  Matrix temp = *this;
+  for (size_t i = 0; i < _size; ++i)
+  {
+   --this->_mat[i];
+  }
+  return temp;
  }
 
  Matrix &Matrix::operator+=(const Matrix &_mat)
  {
   check_Matrix_size(_mat, 1);
-
+  for (size_t i = 0; i < _size; ++i)
+  {
+   _mat[i] += mat._mat[i];
+  }
   return *this;
  }
 
  Matrix &Matrix::operator-=(const Matrix &_mat)
  {
   check_Matrix_size(_mat, 1);
-
+  for (size_t i = 0; i < _size; ++i)
+  {
+   _mat[i] -= mat._mat[i];
+  }
   return *this;
  }
 
  Matrix &Matrix::operator*=(const Matrix &_mat)
  {
   check_Matrix_size(_mat, 2);
-
+  *this = *this * mat;
   return *this;
  }
 
  Matrix &Matrix::operator*=(double num)
  {
-
+  for (size_t i = 0; i < _size; ++i)
+  {
+   if (_mat[i] == 0)
+   {
+    continue;
+   }
+   _mat[i] *= num;
+  }
   return *this;
  }
 
  bool Matrix::operator==(const Matrix &_mat) const
  {
   check_Matrix_size(_mat, 1);
-
+  for (size_t i = 0; i < _size; ++i)
+  {
+   if (_mat[i] != mat._mat[i])
+   {
+    return false;
+   }
+  }
   return true;
  }
 
  bool Matrix::operator!=(const Matrix &_mat) const
  {
   check_Matrix_size(_mat, 1);
-
-  return true;
+  for (size_t i = 0; i < _size; ++i)
+  {
+   if (_mat[i] != mat._mat[i])
+   {
+    return true;
+   }
+  }
+  return false;
  }
 
  bool Matrix::operator>=(const Matrix &_mat) const
  {
   check_Matrix_size(_mat, 1);
-
-  return true;
+  return mat.Matrix_sum() <= this->Matrix_sum();
  }
 
  bool Matrix::operator<=(const Matrix &_mat) const
  {
   check_Matrix_size(_mat, 1);
-
-  return true;
+  return mat.Matrix_sum() >= this->Matrix_sum();
  }
 
  bool Matrix::operator<(const Matrix &_mat) const
  {
   check_Matrix_size(_mat, 1);
-
-  return true;
+  return mat.Matrix_sum() > this->Matrix_sum();
  }
 
  bool Matrix::operator>(const Matrix &_mat) const
  {
   check_Matrix_size(_mat, 1);
-
-  return true;
+  return mat.Matrix_sum() < this->Matrix_sum();
  }
 
  Matrix operator*(int num, Matrix _mat)
  {
+  vector<double> temp((size_t)(mat._size));
+  for (size_t i = 0; i < mat._size; ++i)
+  {
+   temp[i] = mat._mat[i] * num;
+  }
+  return Matrix(temp, mat._row, mat._col);
+ }
 
-  return _mat;
+ int count_row(string str, char to_count)
+ {
+  int count = 0;
+
+  for (size_t i = 0; i < str.size(); i++)
+  {
+   if (str[i] == to_count)
+   {
+    ++count;
+   }
+  }
+
+  return count;
+ }
+ void valid_input(string str)
+ {
+  bool flag = false;
+  for (size_t i = 0; i < str.length(); ++i)
+  {
+   if ((str[i] == ']' && !flag) || (str[i] == '[' && flag))
+   {
+    throw invalid_argument("invalid input ");
+   }
+   if (str[i] == ']' && flag)
+   {
+    flag = false;
+   }
+   if (str[i] == '[')
+   {
+    flag = true;
+   }
+   if ((str[i] == ',' && str[i - 1] != ']') ||
+       (str[i] == ',' && str[i + 1] != ' '))
+   {
+    throw invalid_argument("invalid input ");
+   }
+  }
  }
 
  std::istream &operator>>(std::istream &in, Matrix &_mat)
  {
+  string str;
+  getline(in, str);
+  valid_input(str);
+  int row = count_row(str, ',') + 1;
+  std::vector<double> mat_v;
+  for (size_t i = 0, j = 0; i < str.length(); j = ++i)
+  {
+   while (isdigit(str[i]) != 0)
+   {
+    ++i;
+   }
+   if (i != j)
+   {
+    mat_v.push_back(std::stod(str.substr(j, i - j)));
+   }
+  }
+  mat = Matrix(mat_v, row, int(mat_v.size()) / row);
   return in;
  }
 
  std::ostream &operator<<(std::ostream &out, const Matrix &_mat)
  {
+  int count = 0;
+  for (size_t i = 0; i < mat._size; ++i)
+  {
+   if (count == 0)
+   {
+    out << "[";
+   }
+   out << mat._mat[i];
+   out << ((count != mat._col - 1) ? " " : (i != mat._size - 1) ? "]\n"
+                                                                : "]");
+   count = count == mat._col - 1 ? 0 : ++count;
+  }
   return out;
  }
 }
